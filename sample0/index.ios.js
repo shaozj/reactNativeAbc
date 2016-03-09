d4ca450f244166760702b6a105d75ba8fd39b701
澜嵚 <814184517@qq.com>
2016-03-09 20:07:10 +0800

    sample0

---
+++ sample0/index.ios.js
@@ -0,0 +1,177 @@
+/**
+ * Sample React Native App
+ * 
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+let REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+let MOCKED_MOVIE_DATA = [
+  {
+    title: 'Title', 
+    year: '2015',
+    posters: {
+      thumbnail: 'http://i.imgur.com/UePbdph.jpg'
+    }
+  },{
+    title: 'Title', 
+    year: '2015',
+    posters: {
+      thumbnail: 'http://i.imgur.com/UePbdph.jpg'
+    }
+  },{
+    title: 'Title', 
+    year: '2015',
+    posters: {
+      thumbnail: 'http://i.imgur.com/UePbdph.jpg'
+    }
+  },{
+    title: 'Title', 
+    year: '2015',
+    posters: {
+      thumbnail: 'http://i.imgur.com/UePbdph.jpg'
+    }
+  },{
+    title: 'Title', 
+    year: '2015',
+    posters: {
+      thumbnail: 'http://i.imgur.com/UePbdph.jpg'
+    }
+  },{
+    title: 'Title', 
+    year: '2015',
+    posters: {
+      thumbnail: 'http://i.imgur.com/UePbdph.jpg'
+    }
+  },{
+    title: 'Title', 
+    year: '2015',
+    posters: {
+      thumbnail: 'http://i.imgur.com/UePbdph.jpg'
+    }
+  },{
+    title: 'Title', 
+    year: '2015',
+    posters: {
+      thumbnail: 'http://i.imgur.com/UePbdph.jpg'
+    }
+  },{
+    title: 'Title', 
+    year: '2015',
+    posters: {
+      thumbnail: 'http://i.imgur.com/UePbdph.jpg'
+    }
+  },{
+    title: 'Title', 
+    year: '2015',
+    posters: {
+      thumbnail: 'http://i.imgur.com/UePbdph.jpg'
+    }
+  },{
+    title: 'Title', 
+    year: '2015',
+    posters: {
+      thumbnail: 'http://i.imgur.com/UePbdph.jpg'
+    }
+  },{
+    title: 'Title', 
+    year: '2015',
+    posters: {
+      thumbnail: 'http://i.imgur.com/UePbdph.jpg'
+    }
+  }
+];
+
+class sample0 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: null,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: MOCKED_MOVIE_DATA,
+        });
+      })
+      .done();
+  }
+
+  render() {
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loding movies...
+        </Text>
+      </View>
+    );
+  }
+
+  renderMovie(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.thumbnail}
+          source={{uri: movie.posters.thumbnail}} 
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+let styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  thumbnail: {
+    width: 53,
+    height: 81
+  },
+  rightContainer: {
+    flex: 1
+  },
+  title: {
+    fontSize: 28,
+    marginBottom: 8,
+    textAlign: 'center'
+  },
+  year: {
+    textAlign: 'center'
+  }
+});
+
+AppRegistry.registerComponent('sample0', () => sample0);
+
